d4b76d2a26aeed368008e8a680d62225272a8a4a
Zignuts Parth Gupta <parthg@zignuts.com>
2024-08-01 15:03:19 +0530

    serializers validation added

--- home/serializer.py
+++ home/serializer.py
@@ -1,7 +1,22 @@
+import re
 from rest_framework import serializers
 from .models import Person
 
-class PeopleSerializer(serializers.ModelSerializer) :
-    class Meta :
+class PeopleSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Person
         fields = "__all__"
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Name cannot be empty.")
+
+        if not re.match(r'^[A-Za-z\s]+$', value):
+            raise serializers.ValidationError("Name cannot contain special characters or numbers.")
+
+        return value
+    
+    def validate_age(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Age cannot be negative.")
+        return value
