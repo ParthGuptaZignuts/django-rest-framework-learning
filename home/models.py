5bc856d92767afe45f50a70137379f95b2068901
Zignuts Parth Gupta <parthg@zignuts.com>
2024-08-01 15:38:06 +0530

    foreign key and use of depth keyword and when we want to pass the specify data only

--- home/models.py
+++ home/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Color(models.Model):
+    color_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.color_name
+
 class Person(models.Model):
+    color_name = models.ForeignKey(Color, on_delete=models.CASCADE ,null=True,blank=True,related_name="color")
     name = models.CharField(max_length=100)
     age  = models.IntegerField()
--- home/serializer.py
+++ home/serializer.py
@@ -1,11 +1,19 @@
 import re
 from rest_framework import serializers
-from .models import Person
+from .models import Person, Color
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = ['color_name']
 
 class PeopleSerializer(serializers.ModelSerializer):
+    color = ColorSerializer(read_only=True)
+
     class Meta:
         model = Person
         fields = "__all__"
+        # depth = 1
 
     def validate_name(self, value):
         if not value.strip():
@@ -15,7 +23,7 @@ class PeopleSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Name cannot contain special characters or numbers.")
 
         return value
-    
+
     def validate_age(self, value):
         if value < 0:
             raise serializers.ValidationError("Age cannot be negative.")
